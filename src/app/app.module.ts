31db143ec05148dc10beabd56b31a1b663249ad2
unknown <da.ni.lo_arruda@outlook.com>
2019-02-04 00:33:25 -0300

    Tratando rota não encontrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,12 +21,15 @@ const routes: Routes = [
   { path: 'lancamento/novo', component: LancamentoCadastroComponent },
   { path: 'lancamento/:codigo', component: LancamentoCadastroComponent },
   { path: 'pessoa', component: PessoasPesquisaComponent },
-  { path: 'pessoa/nova', component: PessoaCadastroComponent }
+  { path: 'pessoa/nova', component: PessoaCadastroComponent },
+  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
