c6f8b7d9a5d6c8a7f4ec25213200c2bdb2dec020
unknown <da.ni.lo_arruda@outlook.com>
2019-02-04 00:58:35 -0300

    Utilizando Routing Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'lancamento', pathMatch: 'full' },
+  { path: 'lancamento', component: LancamentosPesquisaComponent },
+  { path: 'lancamento/novo', component: LancamentoCadastroComponent },
+  { path: 'lancamento/:codigo', component: LancamentoCadastroComponent },
+  { path: 'pessoa', component: PessoasPesquisaComponent },
+  { path: 'pessoa/nova', component: PessoaCadastroComponent },
+  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,3 @@
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { AppComponent } from './app.component';
@@ -13,18 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamento', pathMatch: 'full' },
-  { path: 'lancamento', component: LancamentosPesquisaComponent },
-  { path: 'lancamento/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamento/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoa', component: PessoasPesquisaComponent },
-  { path: 'pessoa/nova', component: PessoaCadastroComponent },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -36,7 +21,7 @@ const routes: Routes = [
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
 
     LancamentosModule,
     PessoasModule,
